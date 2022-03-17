47148771729c7024f6a40c24b98ea899faae7075
Muhammad Salah <msalahii@yahoo.com>
2022-03-17 15:47:05 +0200

    usecase and bloc test cases

---
+++ test/features/popular_people_list/domain/usecases/fetch_popular_people_usecase_test.dart
@@ -0,0 +1,43 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:popular_people_app/features/popular_people_list/data/models/popular_people_search_result_model.dart';
+import 'package:popular_people_app/features/popular_people_list/data/models/popular_person_model.dart';
+import 'package:popular_people_app/features/popular_people_list/domain/repositories/popular_people_repo.dart';
+
+@GenerateMocks([PopularPeopleRepository])
+void main() {
+  late FetchPopularPeopleUsecase usecase;
+  late MockPopularPeopleRepository repository;
+
+  setUp(() {
+    repository = MockPopularPeopleRepository();
+    usecase = FetchPopularPeopleUsecase(repository);
+  });
+
+  const searchResults = PopularPeopleSearchResultModel(pageNo: 1, personsList: [
+    PopularPersonModel(
+        personID: 1,
+        name: 'name',
+        imageURL: 'imageURL',
+        department: 'department')
+  ]);
+
+  test(
+    'should fetch popular people list from the repository',
+    () async {
+      // arrange
+      when(repository.fetchPopularPersonsList(1))
+          .thenAnswer((_) async => Right(searchResults));
+      // act
+      final params = FetchPopularPeopleParams(pageNo: 1);
+      final result = await usecase(params);
+
+      // assert
+      expect(result, const Right(searchResults));
+      verify(repository.fetchPopularPersonsList(1));
+      verifyNoMoreInteractions(repository);
+    },
+  );
+}
